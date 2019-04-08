eaa0585436b959b3174e527139c15f48e509a8b8
zigza9 <45957555+zigza9@users.noreply.github.com>
2019-04-08 17:46:49 +0800

    Update threadtrxt.py

--- threadtrxt.py
+++ threadtrxt.py
@@ -3,7 +3,8 @@ from lxml import etree
 from queue import Queue
 import threading
 import json
-# 创建两个列表用于存放线程
+
+
 crawl_list, parse_list = [], []
 
 
@@ -22,16 +23,11 @@ class CrawlThread(threading.Thread):
     def run(self):
         print('%s线程启动' % self.name)
         while True:
-            # 判断条件退出线程
             if self.page_queue.empty():
                 break
-            # 从page队列中提取数据
             page = self.page_queue.get()
-            # 构造url
             url = self.url.format(page)
-            # 返回数据中文乱码，此处将网页源码进行转码
             r = requests.get(url, headers=self.headers).content.decode('utf8')
-            # 将数据存入data对列
             self.data_queue.put(r)
 
 
@@ -44,44 +40,35 @@ class ParseThread(threading.Thread):
         self.lock = lock
 
     def run(self):
-        # 从data对列中提取数据
         data = self.data_queue.get()
-        # 解析内容函数
         self.parse_content(data)
 
     def parse_content(self, data):
-        # xpath解析内容
         tree = etree.HTML(data)
         div_list = tree.xpath('//div[@class="art-t"]')
-        # 创建列表用于存放解析内容
         items = []
         for i in div_list:
-            # 获取内容存入字典
             title = i.xpath('.//h3/a/text()|//div[@class="art-t"]/h3/a/b/text()')
             text = i.xpath('./p/text()')
             item = {
                 'title': title,
                 'text': text,
             }
-            # 将信息写入列表
             items.append(item)
-        # 对文件上锁
+            
         self.lock.acquire()
-        # 写入文件
         self.f.write(json.dumps(items,ensure_ascii=False))
-        # 释放锁
         self.lock.release()
 
 
 def create_queue():
     # 创建队列对象
     page_queue = Queue()
-    # page队列存放数据,数据内容为url的规律变量
     for page in range(1, 5):
         page_queue.put(page)
     # 创建data队列
     data_queue = Queue()
-    # 返回两个队列
+    
     return page_queue, data_queue
 
 
@@ -104,19 +91,17 @@ def create_parse_thread(data_queue, f, lock):
 def main():
     # 创建page队列,data队列
     page_queue, data_queue = create_queue()
-    # 打开文件
+    
     f = open('text.json', 'w', encoding='utf8')
-    # 创建锁
     lock = threading.Lock()
-    # 创建线程
+    
     create_crawl_thread(page_queue, data_queue)
     create_parse_thread(data_queue, f, lock)
-    # 启动线程
+    
     for tcrawl in crawl_list:
         tcrawl.start()
     for tparse in parse_list:
         tparse.start()
-    # 主线程等待子线程结束
     for tcrawl in crawl_list:
         tcrawl.join()
     for tparse in parse_list:
@@ -127,6 +112,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
